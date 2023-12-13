ce095759295ff3cb2d06756ae58cfd5552b4f32c
vinod4848 <vinodku4848@gmail.com>
2023-12-13 16:08:57 +0530

    add News image upload function

--- routes/newRoute.js
+++ routes/newRoute.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const newsController = require('../controller/newController');
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
 
 router.post('/news', newsController.addNews);
 router.get('/news/search', newsController.searchNewsByTitle); 
@@ -8,5 +10,9 @@ router.get('/news', newsController.getAllNews);
 router.get('/news/:id', newsController.getNewsById);
 router.put('/news/:id', newsController.updateNews);
 router.delete('/news/:id', newsController.deleteNews);
-
+router.post(
+    "/uploadImage/news/:id",
+    upload.single("image"),
+    newsController.uploadNewsImage
+  );
 module.exports = router;
