13451fe68eccc40539fa07a6bb85ade8a15fa656
vinod4848 <vinodku4848@gmail.com>
2023-12-20 19:19:36 +0530

    modifiy advertising model

--- models/advertisingModel.js
+++ models/advertisingModel.js
@@ -5,36 +5,13 @@ const advertisingSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-  campaignName: {
+  companyName: {
     type: String,
     required: true,
   },
-  image:{
-    type:String
+  image: {
+    type: String,
   },
-  startDate: {
-    type: Date,
-    required: true,
-  },
-  isApproved: { type: Boolean, default: function () { return this.isPublic; } },
-  endDate: {
-    type: Date,
-    required: true,
-  },
-  clicks: {
-    type: Number,
-    default: 0,
-  },
-  log: [
-    {
-      timestamp: {
-        type: Date,
-        default: Date.now,
-      },
-      event: String,
-      details: String,
-    },
-  ],
 });
 
 const Advertising = mongoose.model("Advertising", advertisingSchema);
--- models/eventModel.js
+++ models/eventModel.js
@@ -6,7 +6,7 @@ const eventsSchema = mongoose.Schema(
     description: { type: String, required: true },
     image: { type: String },
     category: { type: String, require: true },
-    data: { type: String, required: true },
+    date: { type: String, required: true },
     isActive: { type: Boolean, required: true, default: true },
     GalleryId: [{ type: mongoose.Schema.Types.ObjectId, ref: "Gallery" }],
   },
